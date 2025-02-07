f5030b41243d90e9d1ccc741cba82e337d2e993c
Ozy-Viking <admin@hankin.io>
2025-02-07 17:12:40 +1100

    testing actions

--- scripts/gh_pages.sh
+++ scripts/gh_pages.sh
@@ -5,7 +5,7 @@
 for page in "${pages[@]}"; do
     echo $page;
     if [[ -e $page ]]; then
-        sed -e "s/src=\"\/static/src=\".\/static/" -e "s/href=\"\/static/href=\".\/static/" $page | tee $page.tmp 1>/dev/null  
+        sed -e "s/src=\"\/static/src=\".\/static/" -e "s/href=\"\/static/href=\".\/static/" $page | tee $page.tmp # 1>/dev/null  
         mv -f ${page}.tmp ${page}
     fi
 done
