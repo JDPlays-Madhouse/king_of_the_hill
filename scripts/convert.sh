b877f7c351124e2678f3408a282d5803b6de5afe
Ozy-Viking <admin@hankin.io>
2026-03-06 17:29:31 +1100

    feat(weapons): Added the pngs of the new weapons

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,12 +3,18 @@
 set -e
 
 
-file_dir=$1
-
-webp_files=$(ls | grep --color=never .webp)
+file_dir=${1:-"./"}
+done_webp_folder="$file_dir/webp/"
+if [ ! -d "$done_webp_folder" ]; then 
+    mkdir "$done_webp_folder";
+fi
+webp_files=$(ls "$file_dir" | grep --color=never .webp)
 
 for file in $webp_files
 do
-    dwebp $file -o $(echo $file | sed s/\.webp//g).png
+    path="$file_dir/$file"
+    echo "$path"
+    dwebp "$path" -o "$(echo "$path" | sed s/\.webp//g).png"
+    mv "$path" "$done_webp_folder"
 done
 
